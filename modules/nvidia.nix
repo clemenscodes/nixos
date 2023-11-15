58cdc5222efadd630be683581a0b1958bfd8a097
Clemens Horn <clemens.horn@mni.thm.de>
2023-11-15 14:19:42 +0100

    fix(): nvidia.nix sets hyprland nvidia config for zsh

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -19,5 +19,19 @@
       package = config.boot.kernelPackages.nvidiaPackages.stable;
     };
   };
+  programs = {
+    zsh = {
+      loginShellInit = ''
+        export GDK_BACKEND=wayland,x11
+        export GBM_BACKEND=nvidia-drm
+        export SDL_VIDEODRIVER=wayland
+        export CLUTTER_BACKEND=wayland
+        export __GLX_VENDOR_LIBRARY_NAME=nvidia
+        export LIBVA_DRIVER_NAME=nvidia
+        export __GL_GSYNC_ALLOWED=1
+        export __GL_VRR_ALLOWED=1
+      '';
+    };
+  };
 }
 
