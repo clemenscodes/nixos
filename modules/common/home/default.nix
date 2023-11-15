96a41efe40a6270a8c1573ef5be5a8e0983f84a9
Clemens Horn <clemens.horn@mni.thm.de>
2023-11-15 14:26:48 +0100

    fix(): remove manual setupsops from home

--- modules/common/home/default.nix
+++ modules/common/home/default.nix
@@ -10,22 +10,12 @@
     homeDirectory = "/home/${user}";
     sessionPath = [ "/home/${user}/.local/bin" ];
     file = {
-      ".local/bin/setupsops" = {
-        text = ''
-          #!/usr/bin/env bash
-          
-          mkdir -p ~/.config/sops/age
-          nix-shell -p ssh-to-age --run "ssh-to-age -private-key -i ~/.ssh/id_ed25519 > ~/.config/sops/age/keys.txt"
-          nix-shell -p age --run "age-keygen -y ~/.config/sops/age/keys.txt"
-          cd ~/.config/nixos/secrets || exit
-          nix-shell -p sops --run "c sops secrets.yaml"
-        '';
-      };
       ".local/src/README.md" = {
         text = ''
           # Source Code / Packages
           
           - This is the home for all external source code and projects
+          - run `rr` to cd into this directory
         '';
       };
     };
--- modules/common/scripts/default.nix
+++ modules/common/scripts/default.nix
@@ -1,7 +1,9 @@
 { pkgs, ... }: {
-  environment.systemPackages = [
-    (import ./setupsops { inherit pkgs; })
-    (import ./waybar-swaync { inherit pkgs; })
-    (import ./waybar-clock { inherit pkgs; })
-  ];
+  environment = {
+    systemPackages = [
+      (import ./setupsops { inherit pkgs; })
+      (import ./waybar-swaync { inherit pkgs; })
+      (import ./waybar-clock { inherit pkgs; })
+    ];
+  };
 }
