95875fe946781ed47df2b792f48b08613a0f281b
Clemens Horn <clemens.horn@mni.thm.de>
2023-11-23 12:54:50 +0100

    chore(): remove nvidia-offload

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -1,16 +1,4 @@
-{ pkgs, config, lib, ... }:
-let
-  nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
-    export __NV_PRIME_RENDER_OFFLOAD=1
-    export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
-    export __GLX_VENDOR_LIBRARY_NAME=nvidia
-    export __VK_LAYER_NV_optimus=NVIDIA_only
-    exec "$@"
-  '';
-in {
-  environment = {
-    systemPackages = [ nvidia-offload ];
-  };
+{ pkgs, config, lib, ... }: {
   boot = {
     kernelParams = [
       "nvidia.NVreg_PreserveVideoMemoryAllocations=1"
