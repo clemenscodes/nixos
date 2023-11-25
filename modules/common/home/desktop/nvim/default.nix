d5e475e73a8248269d7df00077b966501bb01918
Clemens Horn <clemens.horn@mni.thm.de>
2023-11-25 13:43:04 +0100

    chore(): update flake, start neovim config from scratch

--- modules/common/home/desktop/nvim/default.nix
+++ modules/common/home/desktop/nvim/default.nix
@@ -1,31 +1,24 @@
 { pkgs, ... }: {
-  home = {
-    packages = with pkgs; [
-      nodejs
-      eza
-      ripgrep
-      fd
-      wl-clipboard
-      gcc
-      gnumake
-      tree-sitter
-      wget
-      zip
-      unzip
-      go
-      php82Packages.composer
-      php
-      cargo
-      luajitPackages.luarocks
-      zulu
-      julia
-      (python311.withPackages(ps: with ps; [pip]))
-    ];
-  };
   xdg = {
     configFile = {
       nvim = {
         source = ./config;
+        recursive = true;
+      };
+      "nvim/lua" = {
+        source = ./config/lua;
+        recursive = true;
+      };
+      "nvim/lua/config" = {
+        source = ./config/lua/config;
+        recursive = true;
+      };
+      "nvim/lua/plugins" = {
+        source = ./config/lua/plugins;
+        recursive = true;
+      };
+      "nvim/lua/util" = {
+        source = ./config/lua/util;
         recursive = true;
       };
     };
@@ -37,6 +30,30 @@
       viAlias = true;
       vimAlias = true;
       vimdiffAlias = true;
+      withNodeJs = true;
+      withRuby = true;
+      withPython3 = true;
+      extraPackages = with pkgs; [
+        nodejs
+        eza
+        ripgrep
+        fd
+        wl-clipboard
+        gcc
+        gnumake
+        tree-sitter
+        wget
+        zip
+        unzip
+        go
+        php82Packages.composer
+        php
+        cargo
+        luajitPackages.luarocks
+        zulu
+        julia
+      ];
+      extraPython3Packages = ps: with ps; [ pip ];
     };
   };
 }
