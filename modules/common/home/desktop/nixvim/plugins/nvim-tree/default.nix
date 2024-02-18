98dc154c0dd97ae3b5fe3cfa970297fac1958953
Clemens Horn <clemens.horn@mni.thm.de>
2024-02-18 09:45:37 +0100

    fix(nixvim): nvim tree navigation, on attach

--- modules/common/home/desktop/nixvim/plugins/nvim-tree/default.nix
+++ modules/common/home/desktop/nixvim/plugins/nvim-tree/default.nix
@@ -29,45 +29,46 @@
           };
           onAttach = {
             __raw = ''
-                    function(bufnr)
-                       local api = require("nvim-tree.api")
-                       local function opts(desc)
-                         return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-                       end
+              function(bufnr)
+                local api = require("nvim-tree.api")
+                local function opts(desc)
+                  return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+                end
 
-                       local function edit_or_open()
-                         local node = api.tree.get_node_under_cursor()
+                local function edit_or_open()
+                  local node = api.tree.get_node_under_cursor()
 
-                         if node.nodes ~= nil then
-                           -- expand or collapse folder
-                           api.node.open.edit()
-                         else
-                           -- open file
-                           api.node.open.edit()
-                           -- Close the tree if file was opened
-                           api.tree.close()
-                         end
-                       end
+                  if node.nodes ~= nil then
+                    -- expand or collapse folder
+                    api.node.open.edit()
+                  else
+                    -- open file
+                    api.node.open.edit()
+                    -- Close the tree if file was opened
+                    api.tree.close()
+                  end
+                end
 
-                       -- open as vsplit on current node
-                       local function vsplit_preview()
-                         local node = api.tree.get_node_under_cursor()
+                -- open as vsplit on current node
+                local function vsplit_preview()
+                  local node = api.tree.get_node_under_cursor()
 
-                         if node.nodes ~= nil then
-                           -- expand or collapse folder
-                           api.node.open.edit()
-                         else
-                           -- open file as vsplit
-                           api.node.open.vertical()
-                         end
+                  if node.nodes ~= nil then
+                    -- expand or collapse folder
+                    api.node.open.edit()
+                  else
+                    -- open file as vsplit
+                    api.node.open.vertical()
+                  end
 
-                         -- Finally refocus on tree if it was lost
-                         api.tree.focus()
-                       end
-                       vim.keymap.set("n", "l", edit_or_open,          opts("Edit Or Open"))
-                       vim.keymap.set("n", "L", vsplit_preview,        opts("Vsplit Preview"))
-                       vim.keymap.set("n", "h", api.tree.close,        opts("Close"))
-                       vim.keymap.set("n", "H", api.tree.collapse_all, opts("Collapse All"))
+                  -- Finally refocus on tree if it was lost
+                  api.tree.focus()
+                end
+                api.config.mappings.default_on_attach(bufnr)
+                vim.keymap.set("n", "l", edit_or_open,          opts("Edit Or Open"))
+                vim.keymap.set("n", "L", vsplit_preview,        opts("Vsplit Preview"))
+                vim.keymap.set("n", "h", api.node.navigate.parent_close, opts("Close folder"))
+                vim.keymap.set("n", "H", api.tree.collapse_all, opts("Collapse All"))
               end
             '';
           };
