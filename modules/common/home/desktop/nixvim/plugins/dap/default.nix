b39fdfd4a7f85fa42cd6a0385052200bbe8e163f
Clemens Horn <clemens.horn@mni.thm.de>
2024-02-22 11:55:13 +0100

    feat(nixvim): dap rice

--- modules/common/home/desktop/nixvim/plugins/dap/default.nix
+++ modules/common/home/desktop/nixvim/plugins/dap/default.nix
@@ -6,6 +6,19 @@
         lua
         */
         ''
+          local dap, dapui = require("dap"), require("dapui")
+          dap.listeners.before.attach.dapui_config = function()
+            dapui.open()
+          end
+          dap.listeners.before.launch.dapui_config = function()
+            dapui.open()
+          end
+          dap.listeners.before.event_terminated.dapui_config = function()
+            dapui.close()
+          end
+          dap.listeners.before.event_exited.dapui_config = function()
+            dapui.close()
+          end
           vim.api.nvim_set_hl(0, "red", { fg = "#DD2E44" })
           vim.api.nvim_set_hl(0, "blue", { fg = "#3d59a1" })
           vim.api.nvim_set_hl(0, "green", { fg = "#9ece6a" })
@@ -23,6 +36,35 @@
           extensions = {
             dap-ui = {
               enable = true;
+              controls = {
+                element = "console";
+              };
+              layouts = [
+                {
+                  elements = [
+                    {
+                      id = "scopes";
+                      size = 1;
+                    }
+                  ];
+                  position = "right";
+                  size = 100;                                                     
+                }
+                {
+                  elements = [
+                    {
+                      id = "repl";
+                      size = 0.50;
+                    }
+                    {
+                      id = "console";
+                      size = 0.50;
+                    }
+                  ];
+                  position = "bottom";
+                  size = 15;
+                }
+              ];
             };
             dap-virtual-text = {
               enable = true;
@@ -36,6 +78,14 @@
               b = "Toggle breakpoint";
               c = "Continue";
               t = "Toggle UI";
+              r = "Toggle repl";
+              k = "Kill debugging session";
+            };
+            "<leader>ds" = {
+              name = "+Steps";
+              i = "Step into function call";
+              u = "Step out of function call";
+              o = "Step over function call";
             };
           };
         };
@@ -51,12 +101,57 @@
           };
         }
         {
+          action = ":lua require('dapui').open({reset = true})<CR>";
+          key = "<leader>dr";
+          mode = "n";
+          options = {
+            silent = true;
+            desc = "Reset UI";
+          };
+        }
+        {
           action = ":DapContinue<CR>";
           key = "<leader>dc";
           mode = "n";
           options = {
             silent = true;
             desc = "Continue";
+          };
+        }
+        {
+          action = ":DapTerminate<CR>";
+          key = "<leader>dk";
+          mode = "n";
+          options = {
+            silent = true;
+            desc = "Kill debugging session";
+          };
+        }
+        {
+          action = ":DapStepInto<CR>";
+          key = "<leader>dsi";
+          mode = "n";
+          options = {
+            silent = true;
+            desc = "Step into function call";
+          };
+        }
+        {
+          action = ":DapStepOut<CR>";
+          key = "<leader>dsu";
+          mode = "n";
+          options = {
+            silent = true;
+            desc = "Step out of function call";
+          };
+        }
+        {
+          action = ":DapStepOver<CR>";
+          key = "<leader>dso";
+          mode = "n";
+          options = {
+            silent = true;
+            desc = "Step over function call";
           };
         }
         {
--- modules/common/home/desktop/nixvim/plugins/lsp/default.nix
+++ modules/common/home/desktop/nixvim/plugins/lsp/default.nix
@@ -2,7 +2,53 @@
   imports = [./servers];
   programs = {
     nixvim = {
+      plugins = {
+        lsp = {
+          enable = true;
+          keymaps = {
+            diagnostic = {
+              "<leader>j" = "goto_next";
+              "<leader>k" = "goto_prev";
+            };
+            lspBuf = {
+              K = "hover";
+              gD = "declaration";
+              gr = "references";
+              gd = "definition";
+              gi = "implementation";
+              gt = "type_definition";
+            };
+            silent = true;
+          };
+        };
+        which-key = {
+          registrations = {
+            c = {
+              name = "+LSP";
+              a = "Select code action";
+            };
+          };
+        };
+      };
       keymaps = [
+        {
+          action =
+            /*
+            lua
+            */
+            ''
+              function()
+                  vim.lsp.inlay_hint.enable(0, not vim.lsp.inlay_hint.is_enabled())
+              end
+            '';
+          lua = true;
+          key = "<leader>n";
+          mode = "n";
+          options = {
+            silent = true;
+            desc = "Toggle inlay hints";
+          };
+        }
         {
           action =
             /*
@@ -22,33 +68,6 @@
           };
         }
       ];
-      plugins = {
-        which-key = {
-          registrations = {
-            c = {
-              name = "+LSP";
-              a = "Select code action";
-            };
-          };
-        };
-        lsp = {
-          enable = true;
-          keymaps = {
-            diagnostic = {
-              "<leader>j" = "goto_next";
-              "<leader>k" = "goto_prev";
-            };
-            lspBuf = {
-              K = "hover";
-              gD = "references";
-              gd = "definition";
-              gi = "implementation";
-              gt = "type_definition";
-            };
-            silent = true;
-          };
-        };
-      };
     };
   };
 }
