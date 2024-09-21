864832556e12b712ee91f769adb55f33f03cfbbb
Clemens Horn <clemens.horn@mni.thm.de>
2024-09-21 14:12:00 +0200

    chore(nix): update)
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- modules/home-manager/modules/display/qt/default.nix
+++ modules/home-manager/modules/display/qt/default.nix
@@ -20,35 +20,34 @@ in
     config = mkIf (cfg.enable && cfg.qt.enable) {
       home = {
         packages = with pkgs; [
-          # (catppuccin-kvantum.override {
-          #   accent = "Blue";
-          #   variant = "Macchiato";
-          # })
-          # libsForQt5.qtstyleplugin-kvantum
-          # libsForQt5.qt5ct
-          # libsForQt5.qt5.qtwayland
-          # catppuccin-qt5ct
-          # catppuccin-kvantum
-          # qt6.qtwayland
+          (catppuccin-kvantum.override {
+            accent = "Blue";
+            variant = "Macchiato";
+          })
+          libsForQt5.qtstyleplugin-kvantum
+          libsForQt5.qt5ct
+          libsForQt5.qt5.qtwayland
+          catppuccin-qt5ct
+          catppuccin-kvantum
+          qt6.qtwayland
         ];
         sessionVariables = {
           QT_QPA_PLATFORM = "wayland";
-          # QT_QPA_PLATFORMTHEME = "kvantum";
-          # QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
-          # QT_AUTO_SCREEN_SCALE_FACTOR = "1";
+          QT_QPA_PLATFORMTHEME = "kvantum";
+          QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
+          QT_AUTO_SCREEN_SCALE_FACTOR = "1";
         };
       };
       qt = {
         enable = cfg.qt.enable;
-        # platformTheme = {
-        #   name = "kvantum";
-        # };
+        platformTheme = {
+          name = "kvantum";
+        };
         style = {
-          # name = "kvantum";
-          # package = pkgs.catppuccin-kvantum;
+          name = "kvantum";
+          package = pkgs.catppuccin-kvantum;
           catppuccin = mkIf (osConfig.modules.themes.catppuccin.enable) {
-            # inherit (osConfig.modules.themes.catppuccin) enable flavor;
-            enable = false;
+            inherit (osConfig.modules.themes.catppuccin) enable flavor;
           };
         };
       };
