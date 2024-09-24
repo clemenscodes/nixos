b4fcdc7ad445f00b9925db18294d127f325f81a0
Clemens Horn <clemens.horn@mni.thm.de>
2024-09-24 13:24:54 +0200

    chore(nix): add cache

--- modules/config/nix/default.nix
+++ modules/config/nix/default.nix
@@ -45,6 +45,7 @@ in
             "https://nix-gaming.cachix.org"
             "https://cache.iog.io"
             "https://nixpkgs-unfree.cachix.org"
+            "https://clemenscodes.cachix.org"
           ];
           trusted-public-keys = [
             "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
@@ -53,6 +54,7 @@ in
             "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
             "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
             "nixpkgs-unfree.cachix.org-1:hqvoInulhbV4nJ9yJOEr+4wxhDV4xq2d1DK7S6Nj6rs="
+            "clemenscodes.cachix.org-1:yEwW1YgttL2xdsyfFDz/vv8zZRhRGMeDQsKKmtV1N18="
           ];
         };
       };
