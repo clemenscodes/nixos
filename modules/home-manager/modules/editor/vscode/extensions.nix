942d02d90e08bff266f3ae60a7302d02e6fad9c1
Clemens Horn <clemens.horn@mni.thm.de>
2024-09-29 13:09:08 +0200

    feat(vscode): add option for proprietary

--- modules/home-manager/modules/editor/vscode/extensions.nix
+++ modules/home-manager/modules/editor/vscode/extensions.nix
@@ -1,28 +1,33 @@
 {
+  config,
+  lib,
   inputs,
   system,
   ...
 }: {
   programs = {
     vscode = {
-      extensions = with inputs.nix-vscode-extensions.extensions.${system}.vscode-marketplace; [
-        asvetliakov.vscode-neovim
-        vspacecode.whichkey
-        bbenoist.nix
-        kamadorueda.alejandra
-        pkief.material-icon-theme
-        editorconfig.editorconfig
-        mkhl.direnv
-        tamasfe.even-better-toml
-        serayuzgur.crates
-        rust-lang.rust-analyzer
-        redhat.vscode-yaml
-        moonrepo.moon-console
-        catppuccin.catppuccin-vsc
-        ms-azuretools.vscode-docker
-        exiasr.hadolint
-        ms-vscode-remote.remote-containers
-      ];
+      extensions = with inputs.nix-vscode-extensions.extensions.${system}.vscode-marketplace;
+        [
+          asvetliakov.vscode-neovim
+          vspacecode.whichkey
+          bbenoist.nix
+          kamadorueda.alejandra
+          pkief.material-icon-theme
+          editorconfig.editorconfig
+          mkhl.direnv
+          tamasfe.even-better-toml
+          serayuzgur.crates
+          rust-lang.rust-analyzer
+          redhat.vscode-yaml
+          moonrepo.moon-console
+          catppuccin.catppuccin-vsc
+          ms-azuretools.vscode-docker
+          exiasr.hadolint
+        ]
+        ++ lib.optional config.modules.editor.vscode.proprietary [
+          ms-vscode-remote.remote-containers
+        ];
     };
   };
 }
