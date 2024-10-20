684513989fa610e59f0308748d79143f627b77ec
Clemens Horn <clemens.horn@mni.thm.de>
2024-10-20 19:54:13 +0200

    chore(development): add android hmModule

---
+++ modules/home-manager/modules/development/android/default.nix
@@ -0,0 +1,35 @@
+{inputs}: {
+  pkgs,
+  config,
+  lib,
+  ...
+}: let
+  cfg = config.modules.development;
+in
+  with lib; {
+    import = [inputs.android-nixpkgs.hmModule];
+    options = {
+      modules = {
+        development = {
+          android = {
+            enable = mkEnableOption "Enable android development support" // {default = false;};
+          };
+        };
+      };
+    };
+    config = mkIf (cfg.enable && cfg.android.enable) {
+      android-sdk = {
+        inherit (cfg.android) enable;
+        packages = sdk:
+          with sdk; [
+            platform-tools
+            emulator
+            cmdline-tools-latest
+            ndk-27-2-12479018
+            build-tools-35-0-0
+            platforms-android-35
+            sources-android-35
+          ];
+      };
+    };
+  }
