96ea33a732059d9517578ec96c3968984b3c3a27
Clemens Horn <clemens.horn@mni.thm.de>
2024-10-24 10:23:44 +0200

    chore(nvim): update
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- modules/home-manager/modules/media/communication/element/default.nix
+++ modules/home-manager/modules/media/communication/element/default.nix
@@ -7,6 +7,11 @@
 }: let
   cfg = config.modules.media.communication;
   isDesktop = osConfig.modules.display.gui != "headless";
+  element = pkgs.element-desktop-wayland;
+  elementIcon = pkgs.fetchurl {
+    url = "https://raw.githubusercontent.com/element-hq/element-desktop/refs/heads/develop/res/img/element.png";
+    sha256 = "sha256-FDq3fPk6imYMWxJJnJPH+gBAwBklv+DjHEc42mqmgoU=";
+  };
 in
   with lib; {
     options = {
@@ -22,7 +27,20 @@ in
     };
     config = mkIf (cfg.enable && cfg.element.enable && isDesktop) {
       home = {
-        packages = [pkgs.element-desktop-wayland];
+        packages = [element];
+      };
+      xdg = {
+        desktopEntries = {
+          discord = {
+            name = "Element";
+            type = "Application";
+            categories = ["Network" "InstantMessaging"];
+            exec = "${element}/bin/element-desktop";
+            genericName = "A feature-rich client for Matrix.org";
+            icon = "${elementIcon}";
+            mimeType = ["x-scheme-handler/element"];
+          };
+        };
       };
     };
   }
