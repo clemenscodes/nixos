17ffb20f575e14696ef0cb5618af111a98b7ee5b
Clemens Horn <clemens.horn@mni.thm.de>
2024-11-13 18:03:22 +0100

    feat(nix): enable fetch-closure

--- modules/config/nix/default.nix
+++ modules/config/nix/default.nix
@@ -36,6 +36,7 @@ in
           experimental-features = [
             "nix-command"
             "flakes"
+            "fetch-closure"
           ];
           substituters = [
             "https://cache.nixos.org"
