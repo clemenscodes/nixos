82240257ff21f67980efd98bb8c7348de179ab28
Clemens Horn <clemens.horn@mni.thm.de>
2024-11-16 08:14:56 +0100

    fix(sway-audio-idle-inhibit): use nixpkgs version

--- modules/home-manager/modules/display/lockscreen/sway-audio-idle-inhibit/default.nix
+++ modules/home-manager/modules/display/lockscreen/sway-audio-idle-inhibit/default.nix
@@ -5,46 +5,6 @@
   ...
 }: let
   cfg = config.modules.display.lockscreen;
-  swayAudioIdleInhibit = pkgs.stdenv.mkDerivation {
-    name = "sway-audio-idle-inhibit";
-
-    buildInputs = with pkgs; [
-      cmake
-      pkg-config
-      wayland-protocols
-      libpulseaudio
-      wayland
-      ninja
-    ];
-
-    shellHook = ''
-      export PKG_CONFIG_PATH="${pkgs.pkg-config}/lib/pkgconfig"
-    '';
-
-    dontUseCmakeConfigure = true;
-
-    out = "out";
-
-    src = pkgs.fetchFromGitHub {
-      owner = "ErikReider";
-      repo = "SwayAudioIdleInhibit";
-      rev = "c850bc4812216d03e05083c69aa05326a7fab9c7";
-      sha256 = "02z2nmqhiggvpx66lw9gx7nk5bdc8p2szc37a5zy5ljqkhbz5b1h";
-    };
-
-    configurePhase = ''
-      ${pkgs.meson}/bin/meson build
-    '';
-
-    buildPhase = ''
-      ${pkgs.ninja}/bin/ninja -C build
-    '';
-
-    installPhase = ''
-      mkdir -p $out/bin
-      cp ./build/sway-audio-idle-inhibit $out/bin
-    '';
-  };
 in
   with lib; {
     options = {
@@ -52,7 +12,7 @@ in
         display = {
           lockscreen = {
             sway-audio-idle-inhibit = {
-              enable = mkEnableOption "Enable sway-audio-idle-inhibit" // {default = false;};
+              enable = mkEnableOption "Enable sway-audio-idle-inhibit" // {default = cfg.enable;};
             };
           };
         };
@@ -60,7 +20,7 @@ in
     };
     config = mkIf (cfg.enable && cfg.sway-audio-idle-inhibit.enable) {
       home = {
-        packages = [swayAudioIdleInhibit];
+        packages = [pkgs.sway-audio-idle-inhibit];
       };
     };
   }
