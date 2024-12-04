8db9ffa8471ad4a0ab8c5467e2fdf410abc8d5d0
Clemens Horn <clemens.horn@mni.thm.de>
2024-12-04 02:42:52 +0100

    chore(cardanix): update

--- modules/crypto/cardanix/default.nix
+++ modules/crypto/cardanix/default.nix
@@ -1,46 +1,52 @@
-{inputs, pkgs, ...}: {
+{
+  inputs,
+  pkgs,
+  ...
+}: {
   config,
   lib,
   system,
   ...
-}: let
+}:
+with lib; let
   cfg = config.modules.crypto;
-in
-  with lib; {
-    options = {
-      modules = {
-        crypto = {
-          cardanix = {
-            enable = mkEnableOption "Enable cardanix" // {default = false;};
-          };
+in {
+  options = {
+    modules = {
+      crypto = {
+        cardanix = {
+          enable = mkEnableOption "Enable cardanix" // {default = false;};
         };
       };
     };
-    imports = [inputs.cardanix.nixosModules.${system}];
-
-    config = mkIf (cfg.enable && cfg.cardanix.enable) {
-      services = {
-        cardano-node = {
-          package = pkgs.cardano-node;
-        };
-      };
-      cardano = {
-        enable = true;
-        address = {
-          enable = true;
-        };
-        node = {
-          enable = true;
-        };
-        wallet = {
-          enable = true;
-        };
-        db-sync = {
-          enable = true;
-        };
-        daedalus = {
-          enable = true;
-        };
+  };
+  imports = [
+    inputs.cardanix.nixosModules.${system}
+  ];
+  config = mkIf (cfg.enable && cfg.cardanix.enable) {
+    services = {
+      cardano-node = {
+        package = pkgs.cardano-node;
       };
     };
-  }
+    cardano = {
+      inherit (cfg.cardanix) enable;
+      address = {
+        inherit (cfg.cardanix) enable;
+      };
+      node = {
+        inherit (cfg.cardanix) enable;
+        port = 3001;
+      };
+      wallet = {
+        inherit (cfg.cardanix) enable;
+      };
+      db-sync = {
+        inherit (cfg.cardanix) enable;
+      };
+      daedalus = {
+        inherit (cfg.cardanix) enable;
+      };
+    };
+  };
+}
