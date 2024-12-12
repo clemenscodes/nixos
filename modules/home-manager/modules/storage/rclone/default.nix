e34036a8244adb0154371f2329908e410d76fafe
Clemens Horn <clemens.horn@mni.thm.de>
2024-12-12 12:42:22 +0100

    feat(rclone): sync encrypt

--- modules/home-manager/modules/storage/rclone/default.nix
+++ modules/home-manager/modules/storage/rclone/default.nix
@@ -15,6 +15,14 @@
     echo "type = drive" >> $RCLONE_HOME/${cfg.rclone.gdrive.config}
     echo "team_drive = " >> $RCLONE_HOME/${cfg.rclone.gdrive.config}
 
+    echo >> $RCLONE_HOME/${cfg.rclone.gdrive.config}
+
+    echo "[${cfg.rclone.gdrive.mount}_crypt]" > $RCLONE_HOME/${cfg.rclone.gdrive.config}
+    echo "type = crypt" >> $RCLONE_HOME/${cfg.rclone.gdrive.config}
+    echo "remote = ${cfg.rclone.gdrive.mount}" >> $RCLONE_HOME/${cfg.rclone.gdrive.config}
+    echo "filename_encryption = standard" >> $RCLONE_HOME/${cfg.rclone.gdrive.config}
+    echo "directory_name_encryption = true" >> $RCLONE_HOME/${cfg.rclone.gdrive.config}
+
     ${pkgs.rclone}/bin/rclone \
       --config $RCLONE_HOME/${cfg.rclone.gdrive.config} \
       --vfs-cache-mode writes \
@@ -23,8 +31,21 @@
       --drive-client-id $(${pkgs.bat}/bin/bat ${cfg.rclone.gdrive.clientId} --style=plain) \
       --drive-client-secret $(${pkgs.bat}/bin/bat ${cfg.rclone.gdrive.clientSecret} --style=plain) \
       --drive-token $(${pkgs.bat}/bin/bat ${cfg.rclone.gdrive.token} --style=plain) \
-      ${cfg.rclone.gdrive.mount}: ${cfg.rclone.gdrive.storage} \
+      --crypt-password $(${pkgs.bat}/bin/bat ${cfg.rclone.gdrive.encryption_password} --style=plain) \
+      --crypt-password2 $(${pkgs.bat}/bin/bat ${cfg.rclone.gdrive.encryption_salt} --style=plain) \
+      ${cfg.rclone.gdrive.crypt}: ${cfg.rclone.gdrive.storage} \
       --poll-interval 10m
+  '';
+  syncGoogleDrive = pkgs.writeShellScriptBin "sync-gdrive" ''
+    SYNC_PATH="$HOME/${cfg.rclone.gdrive.sync}"
+    mkdir -p $SYNC_PATH
+
+    while true; do
+      echo "Starting sync at $(date)"
+      ${pkgs.rclone}/bin/rclone sync "${cfg.rclone.gdrive.mount}:" "$SYNC_PATH" --bwlimit=8.5M --progress
+      echo "Sync completed at $(date). Waiting for 10 minutes..."
+      sleep 600
+    done
   '';
 in {
   options = {
@@ -38,6 +59,10 @@ in {
               type = lib.types.str;
               default = "gdrive";
             };
+            crypt = lib.mkOption {
+              type = lib.types.str;
+              default = "${cfg.rclone.gdrive.mount}_crypt";
+            };
             config = lib.mkOption {
               type = lib.types.str;
               default = "${cfg.rclone.gdrive.mount}.conf";
@@ -45,6 +70,10 @@ in {
             storage = lib.mkOption {
               type = lib.types.str;
               default = ".local/share/storage/${cfg.rclone.gdrive.mount}";
+            };
+            sync = lib.mkOption {
+              type = lib.types.str;
+              default = ".local/share/sync/${cfg.rclone.gdrive.mount}";
             };
             clientId = lib.mkOption {
               type = lib.types.path;
@@ -55,6 +84,14 @@ in {
               default = null;
             };
             token = lib.mkOption {
+              type = lib.types.path;
+              default = null;
+            };
+            encryption_password = lib.mkOption {
+              type = lib.types.path;
+              default = null;
+            };
+            encryption_salt = lib.mkOption {
               type = lib.types.path;
               default = null;
             };
@@ -87,7 +124,7 @@ in {
     systemd = {
       user = {
         services = {
-          rclone-gdrive = lib.mkIf cfg.rclone.gdrive.enable {
+          "rclone-${cfg.rclone.gdrive.mount}" = lib.mkIf cfg.rclone.gdrive.enable {
             Unit = {
               Description = "${cfg.rclone.gdrive.mount}";
               After = ["network-online.target"];
@@ -101,6 +138,19 @@ in {
               ExecStop = "${pkgs.fuse}/bin/fusermount -u %h/${cfg.rclone.gdrive.storage}/%i";
               Restart = "always";
               RestartSec = 10;
+            };
+          };
+          "rclone-${cfg.rclone.gdrive.mount}-sync" = lib.mkIf cfg.rclone.gdrive.enable {
+            Unit = {
+              Description = "${cfg.rclone.gdrive.mount} sync";
+              After = ["rclone-${cfg.rclone.gdrive.mount}.service"];
+            };
+            Install = {
+              WantedBy = ["default.target"];
+            };
+            Service = {
+              Type = "simple";
+              ExecStart = lib.getExe syncGoogleDrive;
             };
           };
         };
