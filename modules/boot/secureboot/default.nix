fbaa40f9a306f7a1293e22787850ba1b1752a6ee
Clemens Horn <clemens.horn@mni.thm.de>
2024-12-20 15:59:05 +0100

    chore(boot): update secure boot

--- modules/boot/secureboot/default.nix
+++ modules/boot/secureboot/default.nix
@@ -26,14 +26,6 @@ in {
         enable = true;
         pkiBundle = "/etc/secureboot";
       };
-      loader = {
-        systemd-boot = {
-          enable = lib.mkForce false;
-        };
-        efi = {
-          efiSysMountPoint = "/boot";
-        };
-      };
     };
   };
 }
